cd776e9dc6a555e630d083aa0061d9e27d0be0be
dream <932370912@qq.com>
2021-02-06 12:46:41 +0800

    first commit

---
+++ src/main/java/com/roncoo/eshop/price/service/ProductPriceService.java
@@ -0,0 +1,18 @@
+package com.roncoo.eshop.price.service;
+
+import com.roncoo.eshop.price.model.ProductPrice;
+
+/**
+ * 商品价格service
+ * @author dream
+ *
+ */
+public interface ProductPriceService {
+	public void add(ProductPrice productPrice);
+	
+	public void update(ProductPrice productPrice);
+	
+	public void delete(Long id);
+	
+	public ProductPrice findById(Long id);
+}
